486d0eece052e33bf8c71045e1e2036cc755677b
Rasmus <hansen13579@gmail.com>
2021-01-01 19:58:24 +0100

    Make sure type extensions work

---
+++ RoslynReflection.Test/Models/TypeExtensionsTests.cs
@@ -0,0 +1,80 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using RoslynReflection.Builder;
+using RoslynReflection.Models;
+
+namespace RoslynReflection.Test.Models
+{
+    [TestFixture]
+    public class TypeExtensionsTests
+    {
+        private ScannedModule GetTestModule()
+        {
+            return ModuleBuilder.NewBuilder()
+                .NewNamespace("MyNamespace")
+                .NewClass("MyClass")
+                .NewInnerClass("MyInnerClass")
+                .Finish();
+        }
+        
+        [Test]
+        public void IsNestedType_Yes()
+        {
+            var nested = GetTestModule()
+                .Classes()
+                .Single(c => c.Name == "MyInnerClass");
+
+            Assert.That(nested.IsNestedType(), Is.True); 
+        }
+
+        [Test]
+        public void IsNestedType_No()
+        {
+            var nested = GetTestModule()
+                .Classes()
+                .Single(c => c.Name == "MyClass");
+
+            Assert.That(nested.IsNestedType(), Is.False); 
+        }
+
+        [Test]
+        public void FullName_ForRootClass()
+        {
+            var target = GetTestModule()
+                .Classes()
+                .Single(c => c.Name == "MyClass");
+            
+            Assert.That(target.FullName(), Is.EqualTo("MyClass"));
+        }
+        
+        [Test]
+        public void FullName_ForNestedClass()
+        {
+            var target = GetTestModule()
+                .Classes()
+                .Single(c => c.Name == "MyInnerClass");
+            
+            Assert.That(target.FullName(), Is.EqualTo("MyClass.MyInnerClass"));
+        }
+        
+        [Test]
+        public void FullyQualifiedName_ForRootClass()
+        {
+            var target = GetTestModule()
+                .Classes()
+                .Single(c => c.Name == "MyClass");
+            
+            Assert.That(target.FullyQualifiedName(), Is.EqualTo("MyNamespace.MyClass"));
+        }
+        
+        [Test]
+        public void FullyQualifiedName_ForNestedClass()
+        {
+            var target = GetTestModule()
+                .Classes()
+                .Single(c => c.Name == "MyInnerClass");
+            
+            Assert.That(target.FullyQualifiedName(), Is.EqualTo("MyNamespace.MyClass.MyInnerClass"));
+        }
+    }
+}
---
+++ RoslynReflection/Models/ModuleExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace RoslynReflection.Models
+{
+    public static class ModuleExtensions
+    {
+        public static IEnumerable<ScannedType> Types(this ScannedModule module)
+        {
+            return module.Namespaces.SelectMany(ns => ns.Types);
+        }
+
+        public static IEnumerable<ScannedClass> Classes(this ScannedModule module)
+        {
+            return module.Types().OfType<ScannedClass>();
+        }
+    }
+}
